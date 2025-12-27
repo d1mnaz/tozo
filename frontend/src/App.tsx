3a0b4fc06a407a4065f66e7e26a565ce901f4239
d1mnaz <dimonnaz@gmail.com>
2025-12-27 05:53:43 +0300

    frontend: query and toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,40 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContextProvider } from "./ToastContext";
 import { Helmet, HelmetProvider } from "react-19-helmet-async";
 import Container from "@mui/material/Container";
 import { AuthContextProvider } from "./AuthContext";
 import ThemeProvider from "./ThemeProvider";
 import Router from "./Router";
+import Toasts from "./components/Toasts";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
-      <AuthContextProvider>
-        <HelmetProvider>
-          <Helmet>
-            <title>Tozo</title>
-          </Helmet>
-          <ThemeProvider>
-            <Container maxWidth="md">
-              <Router />
-              <h1>Vite + React</h1>
-            </Container>
-          </ThemeProvider>
-        </HelmetProvider>
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <HelmetProvider>
+            <Helmet>
+              <title>Tozo</title>
+            </Helmet>
+            <ThemeProvider>
+              <ToastContextProvider>
+                <Container maxWidth="md">
+                  <Toasts />
+                  <Router />
+                  <h1>Vite + React</h1>
+                </Container>
+              </ToastContextProvider>
+            </ThemeProvider>
+          </HelmetProvider>
+        </AuthContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
